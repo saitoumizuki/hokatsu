3ab8efaaa8cd27776f8b44894b61d028518b7dbe
saitoumizuki <yoshihiramizuki@gmail.com>
2018-07-12 16:48:35 +0900

    nested修正

--- app/models/nursery.rb
+++ app/models/nursery.rb
@@ -1,7 +1,7 @@
 class Nursery < ApplicationRecord
 	belongs_to :admin
-	has_many :prices
-	accepts_nested_attributes_for :prices, allow_destroy: true
+	has_many :prices, dependent: :destroy
+	accepts_nested_attributes_for :prices, allow_destroy: true, reject_if: :all_blank
 	geocoded_by :address
 	after_validation :geocode
 
