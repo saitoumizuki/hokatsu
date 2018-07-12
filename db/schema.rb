65993606af347db00db48dcedeae96b1bbd8033c
saitoumizuki <yoshihiramizuki@gmail.com>
2018-07-12 23:08:19 +0900

    image投稿実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180710070125) do
+ActiveRecord::Schema.define(version: 20180712133558) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 20180710070125) do
     t.text "time"
     t.text "holiday"
     t.text "url"
-    t.text "image"
+    t.text "image_id"
     t.text "concept"
     t.text "concept_detail"
     t.string "post_code"
