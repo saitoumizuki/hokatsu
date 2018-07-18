5f1e9bdad3bd196a02d7bcbe9567b767f7fabdf2
saitoumizuki <yoshihiramizuki@gmail.com>
2018-07-18 16:51:44 +0900

    スポット機能実装

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -3,9 +3,13 @@ class MemosController < ApplicationController
 		memo = Memo.new(memo_params)
 		memo.user_id = current_user.id
 		memo.save
+		redirect_to nurseries_path(nursery)
 	end
 
 	def update
+		memo = Memo.find(params[:id])
+		memo.update(memo_params)
+		redirect_to nurseries_path(nursery)
 	end
 private
   	def memo_params
