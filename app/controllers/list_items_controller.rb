9d3d7a841dc4ed1bd8eccd03da453abd108f6e9f
saitoumizuki <yoshihiramizuki@gmail.com>
2018-07-19 16:53:50 +0900

    js全然わからない

--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -1,7 +1,7 @@
 class ListItemsController < ApplicationController
 	def index
 		@search = Nursery.ransack(params[:q])
-		@list_items = ListItem.all
+		@list_items = current_user.list_items.all.order("row_order")
 		@memo1 = Memo.find_by(user_id: current_user.id)
 		@memo2 = Memo.new
 		@memo2.user_id = current_user.id
@@ -18,27 +18,40 @@ class ListItemsController < ApplicationController
 		@list_item = ListItem.new
 		@list_itema = ListItem.find_by(nursery_id: @nursery.id, user_id: current_user.id)
 		@user = current_user
+		@search = Nursery.ransack(params[:q])
 		render :template => "nurseries/show"
 	end
 
 	# def edit
 	# end
 
-	# def update
-	# end
+	def update
+		list_item = ListItem.find(params[:id])
+		list_item.update(list_item_params)
+		redirect_to list_items_path
+	end
 
 	def destroy
 		list_item = ListItem.find(params[:id])
+		# destroyの前に@nurseryを取得しておく
 		@nursery = list_item.nursery
 		list_item.destroy
+		# list_itemの並び順を更新する
+		# list_items = current_user.list_items.all
+		# 	list_items.each do |list_item|
+		# 		list_item.row_order = list_items.count + 1
+		# 		list_item.save
+		# 	end
+		# nursery/showを表示する
 		@list_item = ListItem.new
 		@list_itema = ListItem.find_by(nursery_id: @nursery.id, user_id: current_user.id)
 		@user = current_user
+		@search = Nursery.ransack(params[:q])
 		render :template => "nurseries/show"
 	end
 
 private
   	def list_item_params
-      	params.require(:list_item).permit(:user_id, :nursery_id)
+      	params.require(:list_item).permit(:user_id, :nursery_id, :row_order)
   	end
 end
